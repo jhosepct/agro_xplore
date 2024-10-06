59b7646981b77d745ad9257ebb66d41973d671f7
kevin.curinaupa.contractor <kevin.curinaupa.contractor@bbva.com>
2024-10-06 17:55:19 -0500

    modify profile styles

--- lib/screens/profile/view/profile_body.dart
+++ lib/screens/profile/view/profile_body.dart
@@ -8,13 +8,13 @@ import '../cubit/my_user_cubit.dart';
 
 class ProfileBody extends StatefulWidget {
   const ProfileBody({super.key});
+
   @override
   State<ProfileBody> createState() => _ProfileBodyState();
 }
 
 class _ProfileBodyState extends State<ProfileBody> {
   FirebaseProvider firebaseProvider = FirebaseProvider();
-  bool isEditting = false;
   bool isSaving = false;
 
   @override
@@ -25,117 +25,125 @@ class _ProfileBodyState extends State<ProfileBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            color: Theme.of(context).colorScheme.primary,
+            size: 24,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        elevation: 0,
+      ),
       body: isSaving
           ? const Center(child: CircularProgressIndicator())
           : Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 8),
-                    Text(me.name,
-                        style: const TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue)),
-                    const SizedBox(height: 8),
-                    // Visibility(
-                    //   visible: !isEditting,
-                    //   child: FloatingActionButton(
-                    //     onPressed: () {
-                    //       setState(() {
-                    //         isEditting = true;
-                    //       });
-                    //     },
-                    //     child: const Icon(Icons.edit),
-                    //   ),
-                    // ),
-                    ElevatedButton(
-                      style: const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.orange)),
-                      onPressed: () async {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                CupertinoAlertDialog(
-                                  title: const Text('¿Cerrar sesion?'),
-                                  content: const Text(
-                                      '¿Estas seguro que deseas salir de AgroXplore?'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('Cancelar'),
-                                    ),
-                                    ElevatedButton(
-                                      onPressed: () async {
-                                        await context
-                                            .read<AuthCubit>()
-                                            .signOut()
-                                            .then((value) {
-                                          context.read<MyUserCubit>().logOut();
-                                          Navigator.pop(context);
-                                          Navigator.pop(context);
-                                        });
-                                      },
-                                      child: const Text('Si'),
-                                    ),
-                                  ],
-                                ));
-                      },
-                      child: const Text('Cerrar cuenta'),
-                    ),
-                    ElevatedButton(
-                      style: const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.red)),
-                      onPressed: () async {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                CupertinoAlertDialog(
-                                  title: const Text(
-                                      '¿Estas seguro que deseas Eliminar tu cuenta?'),
-                                  content: const Text(
-                                      'Si eliminas tu cuenta se borraran todos tus datos registrados.'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('Cancelar'),
-                                    ),
-                                    ElevatedButton(
-                                      style: const ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStatePropertyAll(
-                                                  Colors.red)),
-                                      onPressed: () async {
-                                        await firebaseProvider.eliminarCuenta();
-                                        // ignore: use_build_context_synchronously
-                                        await context
-                                            .read<AuthCubit>()
-                                            .signOut()
-                                            .then((value) {
-                                          context.read<MyUserCubit>().logOut();
-                                          Navigator.pop(context);
-                                          Navigator.pop(context);
-                                        });
-                                      },
-                                      child: const Text('Eliminar'),
-                                    ),
-                                  ],
-                                ));
-                      },
-                      child: const Text('Eliminar cuenta'),
-                    ),
-                  ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 16),
+                Text(me.name,
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 16),
+                _buildActionCard(
+                  context,
+                  'Cerrar cuenta',
+                  '¿Cerrar sesión de AgroXplore?',
+                  Icons.logout,
+                  Colors.orange,
+                  _signOut,
+                ),
+                const SizedBox(height: 16),
+                _buildActionCard(
+                  context,
+                  'Eliminar cuenta',
+                  'Si eliminas tu cuenta, se borrarán todos tus datos registrados.',
+                  Icons.delete,
+                  Colors.red,
+                  _deleteAccount,
+                ),
+              ],
             ),
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget _buildActionCard(BuildContext context, String title, String content, IconData icon, Color color, Function onTap) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: color,
+          child: Icon(icon, color: Colors.white),
+        ),
+        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(content),
+        trailing: IconButton(
+          icon: const Icon(Icons.arrow_forward),
+          onPressed: () {
+            _showConfirmationDialog(context, title, content, onTap);
+          },
+        ),
+      ),
+    );
+  }
+
+  void _showConfirmationDialog(BuildContext context, String title, String content, Function onTap) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancelar'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              await onTap();
+              Navigator.pop(context);
+            },
+            child: const Text('Confirmar'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _signOut() async {
+    await context.read<AuthCubit>().signOut().then((value) {
+      context.read<MyUserCubit>().logOut();
+      Navigator.pop(context);
+    });
+  }
+
+  Future<void> _deleteAccount() async {
+    await firebaseProvider.eliminarCuenta();
+    await context.read<AuthCubit>().signOut().then((value) {
+      context.read<MyUserCubit>().logOut();
+      Navigator.pop(context);
+    });
   }
 }
